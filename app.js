e0201fea98080f65c70acfb274ff7617d2a6ad51
Anthony-Omo <ajomotayo17@gmail.com>
2026-05-06 16:43:49 +0100

    cors

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const cors = require('cors')
 const app = express()
 const fruits = require("./routes/fruits")
 
+app.use(cors())
+app.use(express.json()) //When we recieve a body it will convert it to json before dealing with it
+
 app.get('/', (req, res) => {
     res.send("Hello from the fruits api")
 })
 
-app.use(cors())
-app.use(express.json()) //When we recieve a body it will convert it to json before dealing with it
+
 app.use('/fruits', fruits) //http://localhost:3000//fruits
 
 module.exports = app
